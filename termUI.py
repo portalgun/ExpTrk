d9806556a55805aa4744095d3c1d5c527e7d1bbd
Hetvi Doshi <hsd37@cornell.edu>
2021-07-27 15:21:20 -0400

    Update termUI.py

--- termUI.py
+++ termUI.py
@@ -1,10 +1,34 @@
 
 ## to allow the terminal to read these commands
 
+<<<<<<< Updated upstream
 import subprocess
 s = subprocess.getstatusoutput(f'ps -ef | grep python3')
 print(s)
 
 import argparse
+=======
+!/usr/bin/env python3
+
+
+import click # importing click library 
+
+@click.group() 
+def progress_bar(): # all the commands to call a progress bar in this group 
+	pass 
+
+
+@progress_bar.command() 
+def overall_progress():
+	# need to call controller here and connect to Meher's script 
+
+
+	
+
+
+
+
+
+>>>>>>> Stashed changes
 
 
