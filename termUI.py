f593233a95865a506e64cab16202da82d6c14036
Hetvi Doshi <hsd37@cornell.edu>
2021-07-28 15:28:53 -0400

    Update termUI.py

--- termUI.py
+++ termUI.py
@@ -1,19 +1,38 @@
 
 ## to allow the terminal to read these commands
 
+## filter out separate arguments to make sure goes cleanly into the controller 
+
 !/usr/bin/env python3
 
 
 import click # importing click library 
+
+## command groups 
 
 @click.group() 
 def progress_bar(): # all the commands to call a progress bar in this group 
 	pass 
 
 
-@progress_bar.command() 
-def overall_progress():
-	# need to call controller here and connect to Meher's script 
+## commands for progress bar 
+
+@click.command() 
+@click.option('--summary', default=0, help='Summary of the progress') 
+@click.argument('progress_type')
+def progress(summary, progress_type): # summary inputs can be: overall, collection, subjects, analysis, publication/writing
+	Controller.parse('progress', summary, progress_type)
+
+
+if __name__ == '__main__':
+    progress()
+
+    
+
+
+
+## commands for summaries 
+
 
 
 	
