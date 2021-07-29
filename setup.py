70d67855d79e96eebc3ceb9b4b5e24c11d0cdbe4
Hetvi Doshi <hsd37@cornell.edu>
2021-07-29 15:19:47 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 
+
 from setuptools import setup
 
 setup(
     name='exptrk',
-    version='0.1',
+    version='0.1.0',
     py_modules=['exptrk'],
     install_requires=[
         'Click',
     ],
-    entry_points='''
-        [console_scripts]
-        exptrk=exptrk:cli
-    ''',
+    entry_points={
+        'console_scripts': [
+            'exptrk = exptrk:random',
+        ],
+    },
 )
-
