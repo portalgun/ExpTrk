82e2410097ea6c34df460a2e3c41e291592c437a
Hetvi Doshi <hsd37@cornell.edu>
2021-07-29 15:19:49 -0400

    Update exptrk.py

--- exptrk.py
+++ exptrk.py
@@ -1,11 +1,13 @@
-#!/usr/bin/env python3
-
 ## to allow the terminal to read these commands
 
 ## filter out separate arguments to make sure goes cleanly into the controller 
 
 
 import click # importing click library 
+
+@click.command()
+def random():
+    click.echo("Hello")
 
 ## command groups 
 
@@ -23,51 +25,48 @@ import click # importing click library
 #     # supporing env vars
 #     cli.main(auto_envvar_prefix='TEST')
 
-
-# @click.command()
-# def random():
-#     @click.echo("Hello")
+# if __name__ == '__main__':
+#     random()   
 
 
 # command for initialization 
 
-@click.command() ## command called when starting a new project with exptrk
-@click.option() ## make the db options 
-@click.argument() 
-def initialize():
-	project_name = click.prompt('Please enter your project name. If it is an existing project, please enter the exact name of it.', type=str)
-	researcher_name = click.prompt('Please enter your name', type=str)
-	stimuli_use = click.prompt('Are you using your own stimuli set', type=bool)
-	Controller.parse(dbuser, dbpassword, dbhost, dbport)
-	Controller.parse(project_name, researcher_name, stimuli_use)
+# @click.command() ## command called when starting a new project with exptrk
+# @click.option() ## make the db options 
+# @click.argument() 
+# def initialize():
+# 	project_name = click.prompt('Please enter your project name. If it is an existing project, please enter the exact name of it.', type=str)
+# 	researcher_name = click.prompt('Please enter your name', type=str)
+# 	stimuli_use = click.prompt('Are you using your own stimuli set', type=bool)
+# 	Controller.parse(dbuser, dbpassword, dbhost, dbport)
+# 	Controller.parse(project_name, researcher_name, stimuli_use)
 
 
-command for project update
+# command for project update
 
-@click.command()
-@click.argument('variable_name', type=str) # variable that needs to be updated 
-@click.argument('update', type=str) # what information needs to be updated
-def update(variable_name, update):
-
-
+# @click.command()
+# @click.argument('variable_name', type=str) # variable that needs to be updated 
+# @click.argument('update', type=str) # what information needs to be updated
+# def update(variable_name, update):
 
 
 
-command for progress bar 
-
-@click.command() 
-@click.option('--summary', default=1, help='Summary of the progress') 
-@click.argument('progress_type')
-def progress(summary, progress_type): # summary inputs can be: overall, collection, subjects, analysis, publication/writing
-	Controller.parse('progress', summary, progress_type)
 
 
-commands for outputs 
+# command for progress bar 
 
-@click.command()
-@click.argument(stimuli_information)
-# if __name__ == '__main__': don't need this since have the setup file available 
-#   progress()
+# @click.command() 
+# @click.option('--summary', default=1, help='Summary of the progress') 
+# @click.argument('progress_type')
+# def progress(summary, progress_type): # summary inputs can be: overall, collection, subjects, analysis, publication/writing
+# 	Controller.parse('progress', summary, progress_type)
+
+
+# commands for outputs 
+
+# @click.command()
+# @click.argument(stimuli_information)
+# if __name__ == '__main__':
+#     random()
 # commands for summaries 
-# options list 
--v - version
+# op
